0e0de33f45bab7ae340ced17fe2a8c06a0762e8a
Marian Adly <marianadly2004@gmail.com>
2025-06-02 20:51:56 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DressStore());
+}
+
+class DressStore extends StatelessWidget {
+  const DressStore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
