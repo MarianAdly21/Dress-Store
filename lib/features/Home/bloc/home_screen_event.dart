407c222026be11d2894f4ec782b555ed3f53d06a
Marian Adly <marianadly2004@gmail.com>
2025-06-04 16:55:36 +0300

    added home screen event and home screen stste

---
+++ lib/features/Home/bloc/home_screen_event.dart
@@ -0,0 +1,12 @@
+class HomeScreenEvent{}
+class LoadedCategoryEvent extends HomeScreenEvent {}
+class LoadedProductEvent extends HomeScreenEvent {}
+class OpenSearchScreenEvent extends HomeScreenEvent {}
+class AddToFavoriteEvent extends HomeScreenEvent {}
+class OpenCartScreenEvent extends HomeScreenEvent {}
+class OpenProfileScreenEvent extends HomeScreenEvent {}
+
+
+
+
+
---
+++ lib/features/Home/bloc/home_screen_state.dart
@@ -0,0 +1,19 @@
+class HomeScreenState {}
+class HomeScreenInitialState extends HomeScreenState {}
+
+
+class OpenCategoryScreenState extends HomeScreenState {}
+class OpenProductScreenState extends HomeScreenState {}
+class OpenSearchScreenState extends HomeScreenState {}
+class OpenCartScreenState extends HomeScreenState {}
+
+class OpenProfileScreenState extends HomeScreenState {}
+
+class ConvertToFavoriteState extends HomeScreenState {}
+
+
+
+
+
+
+
