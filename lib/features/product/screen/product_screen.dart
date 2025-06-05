f0dbea910a78c5cee90840d4c4ceb1138a0cbcc5
Marian Adly <marianadly2004@gmail.com>
2025-06-05 14:08:18 +0300

    add image as a background to scaffold

--- lib/features/product/screen/product_screen.dart
+++ lib/features/product/screen/product_screen.dart
@@ -5,6 +5,29 @@ class ProductScreen extends StatelessWidget {
   final int itemId;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            color: Color(0xffFD8186),
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/item3.webp"),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
   }
 }
