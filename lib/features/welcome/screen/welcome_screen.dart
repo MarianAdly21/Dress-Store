9e540a8da6250bb859ef0340b3c4377ad91d9572
Marian Adly <marianadly2004@gmail.com>
2025-06-20 19:30:54 +0300

    provide ,itegrate and trigger welcome screen bloc

--- lib/features/welcome/screen/welcome_screen.dart
+++ lib/features/welcome/screen/welcome_screen.dart
@@ -1,8 +1,23 @@
+import 'package:dress_store/features/sign_up/screen/sign_up_screen.dart';
+import 'package:dress_store/features/welcome/bloc/welcom_screen_bloc.dart';
 import 'package:dress_store/widgets/button_custom_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => WelcomScreenBloc(),
+      child: const WelcomeScreenWithBloc(),
+    );
+  }
+}
+
+class WelcomeScreenWithBloc extends StatelessWidget {
+  const WelcomeScreenWithBloc({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,21 +31,47 @@ class WelcomeScreen extends StatelessWidget {
             ),
           ),
           Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: ButtonCustomWidget(
-                      backgroundColor: Colors.black,
-                      onTap: () {},
-                      text: "Sign Up"),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: ButtonCustomWidget(onTap: () {}, text: "Login"),
-                ),
-              ],
+            child: BlocListener<WelcomScreenBloc, WelcomScreenState>(
+              listener: (context, state) {
+                if (state is NavigateToLoginScreenState) {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return const SignUpScreen();
+                  }));
+                }
+                if (state is NavigateToSignUpScreenState) {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return const SignUpScreen();
+                  }));
+                }
+              },
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: ButtonCustomWidget(
+                        backgroundColor: Colors.black,
+                        onTap: () {
+                          context
+                              .read<WelcomScreenBloc>()
+                              .add(NavigateToSignUpScreenEvent());
+                        },
+                        text: "Sign Up"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: ButtonCustomWidget(
+                        onTap: () {
+                          context
+                              .read<WelcomScreenBloc>()
+                              .add(NavigateToLoginScreenEvent());
+                        },
+                        text: "Login"),
+                  ),
+                ],
+              ),
             ),
           )
         ],
